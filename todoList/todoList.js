4aba916783156ab4c22a795fc648c38133e6d9f2
mkrasovska <mkrasovska@gmail.com>
2019-01-14 09:06:44 +0200

    todo list homework

---
+++ todoList/todoList.js
@@ -0,0 +1,68 @@
+let todoList = JSON.parse(localStorage.getItem('myTodoList')) || [];
+let listHtml = document.querySelector('.todo-list');
+let noteInput = document.querySelector('.new-note');
+renderList();
+
+function renderList() {
+  while (listHtml.children[0]) {
+    listHtml.children[0].remove();
+  }
+
+  for (let noteNumber in todoList) {
+    let note = todoList[noteNumber];
+    let newListItem = document.createElement('li');
+    listHtml.appendChild(newListItem);
+
+    let newItemDiv = document.createElement('div');
+    newListItem.appendChild(newItemDiv);
+    newItemDiv.classList.add('note-row');
+
+    newItemDiv.innerHTML = `<input type="checkbox">
+                            <label>${note.noteText}</label>
+                            <button class="remove-item"><img src="img/close-cross.png"></button>`;
+
+    let itemButton = newItemDiv.querySelector('button');
+    let itemCheckbox = newItemDiv.querySelector('input');
+    let labelToEdit = newItemDiv.querySelector('label');
+    
+    itemButton.onclick = function() {
+      todoList.splice(noteNumber, 1);
+      renderList();
+    };
+   
+    itemCheckbox.onclick = function () {
+      todoList[noteNumber].done = itemCheckbox.checked;
+      renderList();
+    }
+
+    labelToEdit.ondblclick = function() {
+      let itemToEdit = labelToEdit.closest('li');
+      let editInput = document.createElement('input');
+      let editedNoteText = labelToEdit.innerHTML;
+  
+      itemToEdit.classList.add('hidden');
+      listHtml.insertBefore(editInput, itemToEdit);
+      editInput.value = editedNoteText;
+
+      editInput.onchange = function() {
+        todoList[noteNumber].noteText = editInput.value;
+        renderList();
+      } 
+    } 
+       
+    itemCheckbox.checked = note.done;
+  }
+
+
+  let serialTodoList = JSON.stringify(todoList); 
+  localStorage.setItem('myTodoList', serialTodoList);
+}
+
+noteInput.onchange = function() {
+  if (noteInput.value) {
+    let newNote = { noteText: noteInput.value, done: false };
+    todoList.push(newNote);
+    noteInput.value = '';
+    renderList();
+  }
+}
